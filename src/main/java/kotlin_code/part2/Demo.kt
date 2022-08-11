c494219a960d6775592906adcd3dcdbb46bc2b11
frank.k <frank.k@kakaobank.com>
2022-08-11 11:34:02 +0900

    s

--- src/main/java/kotlin_code/part2/Demo.kt
+++ src/main/java/kotlin_code/part2/Demo.kt
@@ -1,6 +1,6 @@
 package kotlin_code.part2
 
-import com.sun.org.apache.xpath.internal.operations.Bool
+import kotlin_code.part2.Color.*
 
 fun main(args: Array<String>){
 //    println("Hello, world!")
@@ -108,6 +108,26 @@ enum class Color {
 //=====================<Enum 클래스 정의 End>=====================//
 
 //=====================<when 으로 enum 클래스 다루기 Start>=====================//
+fun getMnemonic(color: Color) =
+        when (color) {
+            Color.RED -> "Richard"
+            ORANGE -> "Of"
+            YELLOW -> "York"
+            Color.GREEN -> "Gave"
+            Color.BLUE -> "Battle"
+            Color.INDIGO -> "In"
+            Color.VIOLET -> "Vain"
+        }
+
+// when 분기 조건에 여러 다른 객체 사용하기
+fun mix(c1: Color, c2: Color) =
+        when (setOf(c1,c2)) {
+            setOf(RED, YELLOW) -> ORANGE
+            setOf(YELLOW, BLUE) -> GREEN
+            setOf(BLUE,VIOLET) -> INDIGO
+            else -> throw Exception("Dirty color")
+        }
+
 
 
 //=====================<when 으로 enum 클래스 다루기 End>=====================//
