6f1adeb7ff778357db7789f9080e313c987066a3
rnjstjdgh <gshgsh0831@daum.net>
2023-08-12 13:23:50 +0900

    푸시

--- src/main/java/java_code/TestInterface.java
+++
@@ -1,6 +0,0 @@
-package java_code;
-
-public interface TestInterface {
-
-    boolean isHello(String inputStr);
-}
---
+++ src/main/java/kotlin_code/dummy/Test.java
@@ -0,0 +1,7 @@
+package kotlin_code.dummy;
+
+public class Test {
+    public static void postPhoneComputation(int delay, Runnable runnable){
+        runnable.run();
+    }
+}
